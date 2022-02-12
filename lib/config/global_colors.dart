cfe0ca53e5881dc1d2e7ca912edbe4c49e661cad
JohnM-Kahura <Johnmungai392@gmail.com>
2022-02-12 21:56:37 +0300

    global app colors

---
+++ lib/config/global_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Color backgroundColor = const Color(0xFFE6EAED);
+Color appThemeColor = const Color(0xFFFCDD6D);
+Color textColorBlack = const Color(0xFF000000);
+Color customBlue = const Color(0xFF7AC9F0);
+Color customBlack = const Color(0xFF4E5254);
