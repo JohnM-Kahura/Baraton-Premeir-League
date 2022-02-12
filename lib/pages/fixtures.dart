1df4625ffe46f6945810f411982369cb642a592c
JohnM-Kahura <Johnmungai392@gmail.com>
2022-02-12 21:35:35 +0300

    pages to match with bottom nav

---
+++ lib/pages/fixtures.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Fixtures extends StatefulWidget {
+  const Fixtures({Key? key}) : super(key: key);
+
+  @override
+  _FixturesState createState() => _FixturesState();
+}
+
+class _FixturesState extends State<Fixtures> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/table.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Table extends StatefulWidget {
+  const Table({Key? key}) : super(key: key);
+
+  @override
+  _TableState createState() => _TableState();
+}
+
+class _TableState extends State<Table> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
