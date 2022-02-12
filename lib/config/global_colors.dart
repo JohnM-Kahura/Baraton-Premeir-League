13dfa5bcb61cd4e6db0ea0798ea4f64eece5702e
JohnM-Kahura <Johnmungai392@gmail.com>
2022-02-12 23:27:23 +0300

    global heading style

--- lib/config/global_colors.dart
+++ lib/config/global_colors.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color backgroundColor = const Color(0xFFE6EAED);
 Color appThemeColor = const Color(0xFFFCDD6D);
 Color textColorBlack = const Color(0xFF000000);
 Color customBlue = const Color(0xFF7AC9F0);
 Color customBlack = const Color(0xFF4E5254);
+TextStyle heading = GoogleFonts.poppins(
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
