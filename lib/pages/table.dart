1e74ff4255f356cf81579a7c56e85b56d42ce65b
JohnM-Kahura <Johnmungai392@gmail.com>
2022-02-19 20:21:48 +0300

    table screenshot and sharering

--- lib/pages/table.dart
+++ lib/pages/table.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:bpl/config/global_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:screenshot/screenshot.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
@@ -16,125 +19,164 @@ class BPLTable extends StatefulWidget {
 
 class _BPLTableState extends State<BPLTable> {
   bool showPreviousTables = false;
+  ScreenshotController controller = ScreenshotController();
+  Future<String> saveImage(Uint8List bytes) async {
+    await [Permission.storage].request();
+    final time = DateTime.now()
+        .toIso8601String()
+        .replaceAll('.', '-')
+        .replaceAll(':', '-');
+    final name = 'BPL_Table$time';
+    final result = await ImageGallerySaver.saveImage(bytes, name: name);
+    result['filePath'];
+    return result;
+  }
+
+  Future saveAndShare(Uint8List bytes) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final image = File('${directory.path}/BPL_Table.png');
+    image.writeAsBytes(bytes);
+    await Share.shareFiles([image.path]);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      appBar: AppBar(
-          title: const Center(child: Text('League Table')),
-          elevation: 0,
-          actions: [
-            IconButton(
-              onPressed: () {
-                setState(() {
-                  showPreviousTables = !showPreviousTables;
-                });
-              },
-              icon: const Icon(Icons.event),
-            ),
-          ]),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 20.0, bottom: 10),
-            child: Center(
-              child: Text(
-                '2021/2022.2',
-                style: heading,
-              ),
-            ),
-          ),
-          Container(
-            // margin: const EdgeInsets.all(10),
-            child: Table(
-              columnWidths: Map.from({
-                // TODO: make this more responsive
-                0: FixedColumnWidth(30),
-                1: FixedColumnWidth(105),
-                2: FixedColumnWidth(30),
-                3: FixedColumnWidth(30),
-                4: FixedColumnWidth(30),
-                5: FixedColumnWidth(30),
-                6: FixedColumnWidth(30),
-                7: FixedColumnWidth(30),
-                8: FixedColumnWidth(30),
-              }),
-              border: TableBorder.all(),
-              children: [
-                TableRow(children: [
-                  Text('No.', style: rowHeading, textAlign: center),
-                  Text('Teams', style: rowHeading, textAlign: center),
-                  Text('P', style: rowHeading, textAlign: center),
-                  Text('W', style: rowHeading, textAlign: center),
-                  Text('D', style: rowHeading, textAlign: center),
-                  Text('L', style: rowHeading, textAlign: center),
-                  Text('GD', style: rowHeading, textAlign: center),
-                  Text('Pts', style: rowHeading, textAlign: center),
-                ]),
-                entry('1.', 'Gongagonga', '6', '5', '1', '0', '8', '16'),
-                entry('2.', 'Zambarani', '6', '4', '2', '0', '6', '14'),
-                entry('3.', 'Black Eagles', '6', '5', '1', '0', '8', '13'),
-                entry('4.', 'Nurses', '6', '5', '1', '0', '4', '13'),
-                entry('5.', 'Clippers', '6', '5', '1', '0', '11', '12'),
-                entry('6.', 'Sigalagala', '6', '5', '1', '0', '8', '12'),
-                entry('7.', 'Veterans', '6', '5', '1', '0', '-6', '4'),
-                entry('8.', 'Pirates', '6', '5', '1', '0', '-6', '4'),
-                entry('9.', 'Snipers', '6', '5', '1', '0', '-7', '4  '),
-                entry('10.', 'Rangers', '6', '5', '1', '0', '-6', '3'),
-                entry('11.', 'Rovers', '6', '5', '1', '0', '-9', '3'),
-                entry('12.', 'Yanga', '6', '5', '1', '0', '-12', '3'),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              InkWell(
-                onTap: () async {
-                const   imageUrl ="https://images.unsplash.com/photo-1644641116859-fe0e0e311824?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80";
-                
-                final url=Uri.parse(imageUrl);
-                final response=await http.get(url);
-                final bytes   =response.bodyBytes;
-                final temp =await getTemporaryDirectory();
-                final path = '${temp.path}/BPL_Table.jpg';  
-                File (path).writeAsBytesSync(bytes);
-
-                await Share.shareFiles([path],text: 'Baraton League Table');
-                
+    return Screenshot(
+      controller: controller,
+      child: Scaffold(
+        backgroundColor: backgroundColor,
+        appBar: AppBar(
+            title: const Center(child: Text('League Table')),
+            elevation: 0,
+            actions: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    showPreviousTables = !showPreviousTables;
+                  });
                 },
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Row(
-                    children: const  [
-                      Text('Share',),
-                      Icon(
-                        Icons.share,
-                        size: 15,
-                      )
-                    ],
-                  ),
+                icon: const Icon(Icons.event),
+              ),
+            ]),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 10),
+              child: Center(
+                child: Text(
+                  '2021/2022.2',
+                  style: heading,
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(10)),
-                child:Row(
-                children: const  [Text('Table'),  Icon(Icons.download,size: 20,)],
-              )
-              ),
-            ],
-          )
-        ],
+            ),
+            const customTable(),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: () async {
+                    final image =
+                        await controller.captureFromWidget(const customTable());
+                    saveAndShare(image);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Row(
+                      children: const [
+                        Text(
+                          'Share',
+                        ),
+                        Icon(
+                          Icons.share,
+                          size: 15,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () async {
+                    final image =
+                        await controller.captureFromWidget(const customTable());
+
+                    await saveImage(image);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Row(
+                      children: const [
+                        Text('Table'),
+                        Icon(
+                          Icons.download,
+                          size: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class customTable extends StatelessWidget {
+  const customTable({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      columnWidths: Map.from({
+        // TODO: make this more responsive
+        0: FixedColumnWidth(30),
+        1: FixedColumnWidth(105),
+        2: FixedColumnWidth(30),
+        3: FixedColumnWidth(30),
+        4: FixedColumnWidth(30),
+        5: FixedColumnWidth(30),
+        6: FixedColumnWidth(30),
+        7: FixedColumnWidth(30),
+        8: FixedColumnWidth(30),
+      }),
+      border: TableBorder.all(),
+      children: [
+        TableRow(children: [
+          Text('No.', style: rowHeading, textAlign: center),
+          Text('Teams', style: rowHeading, textAlign: center),
+          Text('P', style: rowHeading, textAlign: center),
+          Text('W', style: rowHeading, textAlign: center),
+          Text('D', style: rowHeading, textAlign: center),
+          Text('L', style: rowHeading, textAlign: center),
+          Text('GD', style: rowHeading, textAlign: center),
+          Text('Pts', style: rowHeading, textAlign: center),
+        ]),
+        entry('1.', 'Gongagonga', '6', '5', '1', '0', '8', '16'),
+        entry('2.', 'Zambarani', '6', '4', '2', '0', '6', '14'),
+        entry('3.', 'Black Eagles', '6', '5', '1', '0', '8', '13'),
+        entry('4.', 'Nurses', '6', '5', '1', '0', '4', '13'),
+        entry('5.', 'Clippers', '6', '5', '1', '0', '11', '12'),
+        entry('6.', 'Sigalagala', '6', '5', '1', '0', '8', '12'),
+        entry('7.', 'Veterans', '6', '5', '1', '0', '-6', '4'),
+        entry('8.', 'Pirates', '6', '5', '1', '0', '-6', '4'),
+        entry('9.', 'Snipers', '6', '5', '1', '0', '-7', '4  '),
+        entry('10.', 'Rangers', '6', '5', '1', '0', '-6', '3'),
+        entry('11.', 'Rovers', '6', '5', '1', '0', '-9', '3'),
+        entry('12.', 'Yanga', '6', '5', '1', '0', '-12', '3'),
+      ],
     );
   }
 }
