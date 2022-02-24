c6b2c898199532b99705ad59597556291d1bf6c3
JohnM-Kahura <Johnmungai392@gmail.com>
2022-02-25 01:17:01 +0300

    stat page ui complete

--- lib/pages/stats.dart
+++ lib/pages/stats.dart
@@ -12,9 +12,43 @@ class _StatsState extends State<Stats> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         backgroundColor: backgroundColor,
-         appBar:AppBar(title:Center(child:Text('Stats'))),
-
+      backgroundColor: backgroundColor,
+      appBar: AppBar(elevation: 0, title: const Center(child: Text('Statistics'))),
+      body: SingleChildScrollView(
+          child: Column(children: [
+        startContainer('John Doe','30','Goals'),
+        startContainer('John Alex','30','Assits'),
+        startContainer('Zambarani','30','Goals'),
+        startContainer('Clippers','30','Clean Sheets'),
+      ])),
     );
+  }
+
+  Container startContainer(String title,String number, String statistic) {
+    return Container(
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Text(title),
+            ),
+            Container(
+              width: double.infinity,
+               padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5), color: Colors.grey),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                
+                children: [Text(number), Text(statistic)],
+              ),
+            )
+          ],
+        ),
+      );
   }
 }
